6f5a409f8a810705e3e9890f6059c2e10b6987ce
12Shrishti <100504253+12Shrishti@users.noreply.github.com>
2023-08-25 04:04:01 +0000

    datatpes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) // we are using nodeJs,not browser
+let name="shrishti";
+let age=23;
+let  isloggedin=false;
+//datatype
+//int,string,boolean,null,undefined,symbol
+//null--->standalone value
+console.log(typeof(null));//object
+console.log(typeof(undefined));//undefined
