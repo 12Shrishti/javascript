5f876b3c8a0df9be603daa8c77e63b45ea8168d1
12Shrishti <100504253+12Shrishti@users.noreply.github.com>
2023-08-28 08:59:56 +0000

    stackheap

---
+++ 01_basics/stackheap.js
@@ -0,0 +1,17 @@
+//Stack..Primitive datatype
+//heap..non-primitive datatype
+//create copies
+let name1="shrishti";
+let name2=name1;
+console.log(name2);
+console.log(name1);
+//******________****** */
+//have refrence
+let obj={
+    name:"shishtu",
+    email:"sris12@gmail.com"
+};
+let obj2=obj;
+obj2.name="shitlu";
+console.log(obj);
+console.log(obj2);
