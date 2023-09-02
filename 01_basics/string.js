61d54a8efb99376b68ce98f03f450d340f74dccf
12Shrishti <100504253+12Shrishti@users.noreply.github.com>
2023-09-02 07:32:29 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,35 @@
+//String interpolation  
+const name="hello"
+const books=5;
+//outdated way to write
+//console.log(name+" "+"i have"+" "+books+"books");
+//New way to write this is
+console.log(`${name}i have ${books} books`);
+const gameName=new String("Shrishti");
+//console.log(gameName[0]);S
+//console.log(gameName[1]);h
+//console.log(gameName.length);8
+//console.log(gameName.__proto__); {}
+//console.log(gameName.toUpperCase()); SHRISHTI
+//console.log(gameName) [String: 'Shrishti']
+//console.log(gameName.charAt(3));i
+//console.log(gameName.charAt(5))h
+//console.log(gameName.indexOf('r'));2
+//console.log(gameName.indexOf('i'));3
+//const newString=gameName.substring(0,4);
+//console.log(newString);shri
+let fruit="apples"
+//console.log(fruit.slice(0,4));appl
+//console.log(fruit.slice(0,3));app
+//console.log(fruit.slice(0,3));
+let newWord="   cuteshrishti";
+//console.log(newWord);    cuteshrishti(contain space)
+//console.log(newWord.trim())cuteshrishti(no space)  
+
+const url="hey123@gmail.com";
+//console.log(url.replace("@","#"));hey123#gmail.com
+//console.log(url.includes("hey")); true
+//console.log(url.includes("shishtu"));false
+
+
+
